2a71305790cda6d38fff6816f6d593212ca5e97e
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-05 13:32:19 -0800

    Create first test

---
+++ src/test/java/models/EventTest.java
@@ -0,0 +1,22 @@
+package models;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 1/5/18.
+ */
+public class EventTest {
+
+    @Test
+    public void returnEvent_returnFirstAttributes_HashMap(){
+        Event testEvent = new Event();
+        HashMap<String,Integer> expectedOutput = new HashMap<String,Integer>();
+        expectedOutput.put(Guest,1);
+        assertEquals(expectedOutput,testEvent.returnEvent(Guest,1));
+    }
+
+}
